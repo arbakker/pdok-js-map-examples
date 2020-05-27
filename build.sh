b425178b5c120608a69b788298a3f18b4ff14230
Anton Bakker <anton.bakker@kadaster.nl>
2020-05-27 19:10:07 +0200

    updated build script

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ app_li=""
 while read -r DIR; do 
     rm -r "$DIR/dist"
     npm run-script build --prefix "$DIR"
-    rm -rf docs/$(basename "$DIR")
-    cp -r "$DIR/dist" docs/$(basename "$DIR")
+    rm -rf gh-pages/$(basename "$DIR")
+    cp -r "$DIR/dist" gh-pages/$(basename "$DIR")
     app_li="${app_li}<li><a href=\"$(basename "$DIR")/index.html\">$(basename "$DIR")</a></li>"
 done< <(find . -maxdepth  1 -type d -regextype posix-egrep   -regex ".*(leaflet|openlayers).*" | sort)
 app_ul="<ul>${app_li}</ul>"
-sed "s#{{ applications_list }}#${app_ul}#g" index/index.html > docs/index.html
-cp index/*.css docs/
+sed "s#{{ applications_list }}#${app_ul}#g" index/index.html > gh-pages/index.html
+cp index/*.css gh-pages/
