d58456250bf8e6298b510e92d66c8e334803256b
Anton Bakker <anton.bakker@kadaster.nl>
2020-12-15 09:25:07 +0100

    added comment on tilematrix

--- openlayers-wmts-epsg28992/index.js
+++ openlayers-wmts-epsg28992/index.js
@@ -20,6 +20,8 @@ const rdProjection = new Projection({
   extent: [-285401.92, 22598.08, 595401.92, 903401.92]
 })
 
+// can be calculated based on resolution z0, written out for clarity
+// see https://www.geonovum.nl/uploads/standards/downloads/nederlandse_richtlijn_tiling_-_versie_1.1.pdf
 const resolutions = [3440.640, 1720.320, 860.160, 430.080, 215.040, 107.520, 53.760, 26.880, 13.440, 6.720, 3.360, 1.680, 0.840, 0.420, 0.210]
 const matrixIds = new Array(15)
 for (var i = 0; i < 15; ++i) {
@@ -68,7 +70,7 @@ const map = new Map({
 })
 
 var layerSwitcher = new LayerSwitcher({
-  tipLabel: 'Legend', // Optional label for button
-  groupSelectStyle: 'none' // Can be 'children' [default], 'group' or 'none'
+  tipLabel: 'Legend',
+  groupSelectStyle: 'none'
 })
 map.addControl(layerSwitcher)
