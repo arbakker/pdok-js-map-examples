9fc0136bfa7451b936bc333cca68cad3bbfdc045
Anton Bakker <anton.bakker@kadaster.nl>
2021-09-24 15:44:02 +0200

    updated parcel bundler

--- maplibregl-mvt-epsg3857/index.js
+++ maplibregl-mvt-epsg3857/index.js
@@ -1,12 +1,12 @@
-import 'ol/ol.css'
+import 'maplibre-gl/dist/maplibre-gl.css'
 
 import { Map } from 'maplibre-gl'
 import style from './public/style.json'
 
-var map = new Map({
+// eslint-disable-next-line no-unused-vars
+const map = new Map({
   container: 'map', // container id
   style: style, // style URL
   center: [5.473700, 52.099804], // starting position [lng, lat]
   zoom: 7 // starting zoom
 })
-
